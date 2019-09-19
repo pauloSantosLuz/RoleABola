9375147e4079cfc898b94d915d77c5e7183bce78
Paulo Henrique Luz <paulohenrique.luz@hotmail.com>
2019-09-19 00:32:52 -0300

    Checkpoint aula 17/09/19

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject jogador;
+    Vector3 diferencaBolaCamera;
+
+    void Start () 
+    {
+        diferencaBolaCamera =
+         jogador.transform.position - transform.position;
+    }
+
+    void LateUpdate() {
+        transform.position =
+          jogador.transform.position - diferencaBolaCamera;
+    }
+
+}
---
+++ Assets/Scripts/Jogador.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    Vector3 forcaAplicada;
+
+    public float intensidade = 6f;
+
+   
+    void Start()
+    {
+       rigidbody = GetComponent<Rigidbody>();
+    }
+  void FixedUpdate()
+  {
+      float horizontal = Input.GetAxis("Horizontal");
+      float vertical = Input.GetAxis("Vertical");
+
+      forcaAplicada = new Vector3(horizontal, 0f, vertical);
+
+      rigidbody.AddForce(forcaAplicada * intensidade);
+  }
+}
